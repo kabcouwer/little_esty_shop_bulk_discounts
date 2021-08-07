ff88339619ff8f28a3b9ddef015db51e48848610
Kim Abcouwer <kimberly.abcouwer@gmail.com>
2021-08-07 15:27:10 -0600

    Add tests to merchant dashboard index spec

--- spec/features/dashboard/index_spec.rb
+++ spec/features/dashboard/index_spec.rb
@@ -115,4 +115,14 @@ RSpec.describe 'merchant dashboard' do
   it "shows the date that the invoice was created in this format: Monday, July 18, 2019" do
     expect(page).to have_content(@invoice_1.created_at.strftime("%A, %B %-d, %Y"))
   end
+
+  it 'has a link to view all a merchants discounts' do
+    expect(page).to have_link('Discounts')
+  end
+
+  it 'has a usable discount link that redirects to its index page' do
+    click_link('Discounts')
+
+    expect(current_path).to eq("/merchant/#{@merchant1.id}/discounts")
+  end
 end
