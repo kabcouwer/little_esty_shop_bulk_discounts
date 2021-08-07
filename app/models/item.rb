a6d8c16debeabf405576278a2a747c147a8656a8
Kim Abcouwer <kimberly.abcouwer@gmail.com>
2021-08-07 11:37:55 -0600

    Created conditionals for best day methods in item and merchant models

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,14 +11,23 @@ class Item < ApplicationRecord
   enum status: [:disabled, :enabled]
 
   def best_day
-    invoices
-    .joins(:invoice_items)
-    .where('invoices.status = 2')
-    .select('invoices.*, sum(invoice_items.unit_price * invoice_items.quantity) as money')
-    .group(:id)
-    .order("money desc", "created_at desc")
-    .first
-    .created_at
-    .to_date
+    if invoices
+      .joins(:invoice_items)
+      .where('invoices.status = 2')
+      # .select('invoices.*, sum(invoice_items.unit_price * invoice_items.quantity) as money')
+      # .group(:id)
+      # .order("money desc", "created_at desc")
+      .empty?
+      return 0
+    else invoices
+      .joins(:invoice_items)
+      .where('invoices.status = 2')
+      .select('invoices.*, sum(invoice_items.unit_price * invoice_items.quantity) as money')
+      .group(:id)
+      .order("money desc", "created_at desc")
+      .first
+      .created_at
+      .to_date
+    end
   end
 end
