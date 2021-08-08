e6a601dbd9265f5789e742fd8ab549cde48597b2
Kim Abcouwer <kimberly.abcouwer@gmail.com>
2021-08-08 16:11:06 -0600

    Add swagger data method to discounts controller

--- app/controllers/discounts_controller.rb
+++ app/controllers/discounts_controller.rb
@@ -1,5 +1,6 @@
 class DiscountsController < ApplicationController
   before_action :get_merchant, only: [:index, :show]
+  before_action :swagger_data, only: [:index]
   def index
     @discounts = @merchant.bulk_discounts
   end
@@ -9,6 +10,9 @@ class DiscountsController < ApplicationController
   end
 
   private
+  def swagger_data
+    @holidays = SwaggerData.holidays
+  end
   def get_merchant
     @merchant = Merchant.find(params[:merchant_id])
   end
