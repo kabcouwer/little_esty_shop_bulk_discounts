e6fb9c2a8076efd0fe9a3bd46d25076971a1ca89
Kim Abcouwer <kimberly.abcouwer@gmail.com>
2021-08-08 14:01:43 -0600

    Add show and get_merchant to discount controller

--- app/controllers/discounts_controller.rb
+++ app/controllers/discounts_controller.rb
@@ -1,6 +1,15 @@
 class DiscountsController < ApplicationController
+  before_action :get_merchant, only: [:index, :show]
   def index
-    @merchant = Merchant.find(params[:merchant_id])
     @discounts = @merchant.bulk_discounts
+  end
+
+  def show
+    @discount = BulkDiscount.find(params[:id])
+  end
+
+  private
+  def get_merchant
+    @merchant = Merchant.find(params[:merchant_id])
   end
 end
