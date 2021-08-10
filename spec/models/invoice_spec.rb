fe5e511a9c3c61f9bc8a9858ca02e1fec39673f1
Kim Abcouwer <kimberly.abcouwer@gmail.com>
2021-08-09 18:01:50 -0600

    Update test

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -11,8 +11,9 @@ RSpec.describe Invoice, type: :model do
     it { should have_many(:merchants).through(:items) }
     it { should have_many :transactions}
   end
+
   describe "instance methods" do
-    it "total_revenue" do
+    it "#total_revenue" do
       @merchant1 = Merchant.create!(name: 'Hair Care')
       @item_1 = Item.create!(name: "Shampoo", description: "This washes your hair", unit_price: 10, merchant_id: @merchant1.id, status: 1)
       @item_8 = Item.create!(name: "Butterfly Clip", description: "This holds up your hair but in a clip", unit_price: 5, merchant_id: @merchant1.id)
